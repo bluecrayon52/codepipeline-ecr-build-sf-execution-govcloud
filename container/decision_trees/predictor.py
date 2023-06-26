61e04a5b7be28ed1ac33c95ba8fdaaefab2363a9
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2023-06-26 17:57:09 -0400

    testing pipeline trigger

--- container/decision_trees/predictor.py
+++ container/decision_trees/predictor.py
@@ -1,5 +1,6 @@
 # This is the file that implements a flask server to do inferences. It's the file that you will modify to
 # implement the scoring for your own algorithm.
+#Testing
 
 from __future__ import print_function
 
